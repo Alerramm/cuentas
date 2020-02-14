59b62f59b999e4b4410813ac0614b186ddc3cdb7
Alerramm <aleramm117@gmail.com>
2020-02-14 14:15:22 -0600

    temp fechas vencimiento

--- src/containers/Cuentas/Cuentas.js
+++ src/containers/Cuentas/Cuentas.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Layout, Table } from 'antd';
 import { consultaClientes, consultaFacturas } from './CuentasActions';
+import moment from 'moment';
 const { Content } = Layout;
 class Cuentas extends Component {
 	state = {
@@ -15,6 +16,7 @@ class Cuentas extends Component {
 			{ title: 'Viaje', dataIndex: 'viaje', key: 'viaje' },
 			{ title: 'Monto', dataIndex: 'monto', key: 'monto' },
 			{ title: 'Fecha', dataIndex: 'fecha', key: 'fecha' },
+			{ title: 'Fecha Vencimiemto', dataIndex: 'fechaVencimiento', key: 'fechaVencimiento' },
 		];
 		const facturas = data.filter(element => element.cliente === record.cliente);
 		return <Table columns={columns} dataSource={facturas[0].facturas} pagination={false} />;
@@ -42,6 +44,7 @@ class Cuentas extends Component {
 			},
 			{ title: 'SEMANA 1', dataIndex: 'semana1', key: 'semana1' },
 			{ title: 'SEMANA 2', dataIndex: 'semana2', key: 'semana2' },
+			{ title: 'DIAS DE CREDITO', dataIndex: 'diasCredito', key: 'diasCredito' },
 		];
 
 		consultaClientes().then(response => {
@@ -49,11 +52,12 @@ class Cuentas extends Component {
 			clientes.map((item, index) => {
 				data.push({
 					key: 'C' + index,
-					cliente: item,
+					cliente: item.nombre,
 					numeroDeFacturasPorPagar: 0,
 					montoTotalPorPagar: 0,
 					semana1: '',
 					semana2: '',
+					diasCredito: item.diasCredito,
 					facturas: [],
 				});
 				return item;
@@ -67,7 +71,29 @@ class Cuentas extends Component {
 			const Facturas = response.payload;
 			Facturas.map((item, index) => {
 				data.map(element => {
+					console.log(element);
 					if (element.cliente === item.cliente) {
+						const months = [
+							'ENE',
+							'FEB',
+							'MAR',
+							'ABR',
+							'MAY',
+							'JUN',
+							'JUL',
+							'AGO',
+							'SEP',
+							'OCT',
+							'NOV',
+							'DIC',
+						];
+						console.log(moment());
+						console.log(item.fecFacturacion);
+						const fecha = new Date();
+						console.log('fecha x');
+						console.log(item.fecFacturacion);
+						let fechaVencimiento = new Date(fecha);
+						fechaVencimiento.setDate(fecha.getDate() + element.diasCredito);
 						element.numeroDeFacturasPorPagar = element.numeroDeFacturasPorPagar + 1;
 						element.montoTotalPorPagar =
 							element.montoTotalPorPagar + parseInt(item.monto, 10);
@@ -78,6 +104,7 @@ class Cuentas extends Component {
 							monto: item.monto,
 							factura: item.factura,
 							fecha: item.fecFacturacion,
+							fechaVencimiento: fecha.setTime(Date.parse(item.fecFacturacion)),
 						});
 					}
 					return element;
