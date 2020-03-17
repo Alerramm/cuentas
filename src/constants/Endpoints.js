4cbfc5bd13498520064c4c0e8ec388f58520f22c
Alejandro Galicia <out_agaliciag@independencia.com.mx>
2020-03-17 13:12:13 -0600

    add endpoints correctos

--- src/constants/Endpoints.js
+++ src/constants/Endpoints.js
@@ -1,9 +1,9 @@
 /** CONSULTA CLIENTES */
 export const CONSULTAR_CLIENTES =
-	'http://www.misistema.mx/beluga/Finanzas/endpoints/cuentas/get/clientes.php';
+	'https://cors-anywhere.herokuapp.com/http://www.misistema.mx/belugaQA/Finanzas/endpoints/cuentas/get/clientes.php';
 /** CONSULTA FACTURAS */
 export const CONSULTAR_FACTURAS =
-	'http://www.misistema.mx/beluga/Finanzas/endpoints/cuentas/get/facturas.php';
+	'https://cors-anywhere.herokuapp.com/http://www.misistema.mx/belugaQA/Finanzas/endpoints/cuentas/get/facturas.php';
 /** ACTUALIZA ESTATUS FACTURA */
 export const ACTUALIZA_ESTATUS_FACTURAS =
-	'https://cors-anywhere.herokuapp.com/http://www.misistema.mx/beluga/Finanzas/endpoints/cuentas/put/abonar.php';
+	'https://cors-anywhere.herokuapp.com/http://www.misistema.mx/belugaQA/Finanzas/endpoints/cuentas/put/abonar.php';
